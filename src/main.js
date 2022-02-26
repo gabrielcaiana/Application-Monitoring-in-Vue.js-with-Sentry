0e21ad63c950132cd65b03311a93f557ccf957cd
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-02-26 11:26:40 -0300

    install sentry in project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import "@/assets/main.pcss"
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 
-createApp(App)
-.use(router)
-.mount('#app')
+import "@/assets/main.pcss";
+
+const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: "https://f3a97c9b0123433ca5d70f88d2e497d5@o1127381.ingest.sentry.io/6233546",
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+app.use(router);
+app.mount("#app");
