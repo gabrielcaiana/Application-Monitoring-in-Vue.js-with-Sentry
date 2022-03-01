7cc646ab5438635e9927384659ca82dc88eaa75d
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-03-01 13:26:29 -0300

    creation function to force an error

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <h1 class="text-4xl">About</h1>
-    <button @click="user.email.send()">Send email</button>
+    <button @click="user.email.send()">Send email</button><br />
+    <button @click="makeToast">Make toast</button>
   </div>
 </template>
 
 <script setup>
 const user = {};
+const makeToast = () => {
+  sliceBread();
+};
 </script>
